40c4646e5a29705cbf6b6c1a7fb9928eeb75e329
root <zmainpro@gmail.com>
2025-06-22 14:47:40 +0200

    Facade for user

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,4 +1,5 @@
 from app.persistence.repository import InMemoryRepository
+from app.models import User
 
 class HBnBFacade:
     def __init__(self):
@@ -8,7 +9,17 @@ class HBnBFacade:
         self.amenity_repo = InMemoryRepository()
 
     def create_user(self, user_data):
-        pass
+        user = User(**user_data)
+        self.user_repo.add(user)
+        return user
 
-    def get_place(self, place_id):
-        pass
+    def get_user(self, user_id):
+        return self.user_repo.get(user_id)
+
+    def get_user_by_email(self, email):
+        return self.user_repo.get_by_attribute('email', email)
+
+    def get_all_users(self):
+        """Retrieve all users from the database"""
+        return self.user_repo.get_all()
+
