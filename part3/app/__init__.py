55699e5c4e3a102e68f5b2ec4536221ba6b01431
root <zmainpro@gmail.com>
2025-07-03 11:22:42 +0200

    Add db and jwt manager

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
 from flask_restx import Api
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 
+db = SQLAlchemy()
+jwt = JWTManager()
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
@@ -21,5 +25,8 @@ def create_app(config_class="config.DevelopmentConfig"):
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
+
+    db.init_app(app)
+    jwt.init_app(app)
 
     return app
